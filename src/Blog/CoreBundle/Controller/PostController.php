1436cee6dd5cf4dd4921c2cf9c25b5d05f01c82d
kanat <taalayuulu@inbox.ru>
2016-03-08 11:11:04 +0600

    create new _post.html.twig

--- src/Blog/CoreBundle/Controller/PostController.php
+++ src/Blog/CoreBundle/Controller/PostController.php
@@ -4,16 +4,19 @@ namespace Blog\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class PostController extends Controller
 {
     /**
+     * @return array
      * @Route("/")
+     * @Template()
      */
     public function indexAction()
     {
+        $posts = $this->getDoctrine()->getRepository('ModelBundle:Post')->findAll();
         return $this->render('CoreBundle:Post:index.html.twig', array(
-            // ...
+            'post'=>$posts
         ));
     }
 
--- src/Blog/CoreBundle/Tests/Controller/PostControllerTest.php
+++ src/Blog/CoreBundle/Tests/Controller/PostControllerTest.php
@@ -12,6 +12,8 @@ class PostControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/');
         $this->assertTrue($client->getResponse()->isSuccessful(),'The responce was not successfull ');
+        $this->assertCount(3, $crawler->filter('h2'),'There should be 3 displayed posts');
     }
+
 
 }
