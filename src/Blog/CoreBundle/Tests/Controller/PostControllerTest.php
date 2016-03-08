60373d15a9368a1fabb4318a843b3db6b0234653
kanat <taalayuulu@inbox.ru>
2016-03-08 21:55:36 +0600

    create new function in Post tester faile

--- src/Blog/CoreBundle/Tests/Controller/PostControllerTest.php
+++ src/Blog/CoreBundle/Tests/Controller/PostControllerTest.php
@@ -29,4 +29,32 @@ class PostControllerTest extends WebTestCase
         $this->assertEquals($post->getTitle(),$crawler->filter('h1')->text(), 'invalid post title');
         $this->assertGreaterThanOrEqual(1, $crawler->filter('article.comment')->count(),'There should be at least 1 comment lan!');
     }
+
+    /**
+     * Test  create a comment
+     */
+    public function testCreateComment()
+    {
+        $client = static::createClient();
+        /** @var Post $post */
+        $post = $client->getContainer()->get('doctrine')->getManager()->getRepository('ModelBundle:Post')->findFirst();
+        $crawler = $client->request('GET','/'.$post->getSlug());
+        $buttonCrawlerNode=$crawler->selectButton('Send');
+        $form  = $buttonCrawlerNode->from(array(
+            'blog_modelbundle_comment[authorName]' => 'A humble commenter',
+            'blog_modelbundle_comment[body]' => 'Hi , this is simple comment ',
+        ));
+        $client->submit($form);
+
+        $this->assertTrue(
+            $client->getResponse()->isRedirect('/'.$post->getSlug()) ,
+            'There was not  redirection after submit ! (ulan gine unuttun)'
+        );
+        $crawler=$client->followRedirect();
+        $this->assertCount(
+          1,
+          $crawler->filter('html:contains("Your comment was comment")'),
+            'There was not any comfirmation message (tekrar yaz lan)'
+        );
+    }
 }
