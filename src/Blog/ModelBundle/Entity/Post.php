c72afe908f032a42fddd11773260fd91dae7823a
kanat <taalayuulu@inbox.ru>
2016-03-08 13:42:30 +0600

    generate new entities in ModelBundle:Post

--- src/Blog/ModelBundle/Entity/Post.php
+++ src/Blog/ModelBundle/Entity/Post.php
@@ -4,6 +4,7 @@ namespace Blog\ModelBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Gedmo\Mapping\Annotation as Gedmo;
 /**
  * Post
  *
@@ -28,7 +29,12 @@ class Post extends Timestampable
      *
      */
     private $title;
-
+    /**
+     * @var string
+     * @Gedmo\Slug(fields={"title"}, unique=false)
+     * @ORM\Column(length=255)
+     */
+    private $slug;
     /**
      * @var string
      *
@@ -128,4 +134,28 @@ class Post extends Timestampable
     }
 
 
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     *
+     * @return Post
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
 }
